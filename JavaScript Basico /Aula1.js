cc5f1ed1efe2d2d186f8f00ba38a12092ab8a50c
Vinicius Valim <viniciusvalim@minidevinicius.cbnet.com.br>
2022-07-04 16:46:48 -0300

    Aula 1 completa

---
+++ JavaScript Basico /Aula1.js
@@ -0,0 +1,3 @@
+let x = prompt("Qual numero que você quer somar?")
+let y = prompt("Qual numero você quer somar com o anterior")
+alert ("A resposta é:" + (x + y))
--- JavaScript Basico /script.js
+++
@@ -1,3 +0,0 @@
-prompt ("Qual o seu nome ?")
-alert("Olá mundo, [nome do usuário]")
- 

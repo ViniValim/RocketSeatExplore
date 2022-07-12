61a93e3eea036b8fcfc25e6367d5d094ce062ab7
Vinicius Valim <viniciusvalim@minidevinicius.cbnet.com.br>
2022-07-12 09:07:11 -0300

    Aula 3 terminada

---
+++ JavaScript Basico /Aulas terminadas/Aula3.js
@@ -0,0 +1,17 @@
+let fisrtNumber = prompt("Digite o primeiro número")
+let secondNumber = prompt("Digite o segundo número")
+
+fisrtNumber = Number(fisrtNumber)
+secondNumber = Number(secondNumber)
+
+const sum = fisrtNumber + secondNumber
+const sub = fisrtNumber - secondNumber
+const mult = fisrtNumber * secondNumber
+const div = fisrtNumber / secondNumber
+const restDiv = FileReader % secondNumber
+
+alert('Soma :' + sum)
+alert('Subtração :' + sub)
+alert('Multplicação :' + mult)
+alert('Divisão :' + div)
+alert('Resto da divisão :' + restDiv)

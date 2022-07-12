8064678b802e2b2a5f6423ee3d0da5a53dd3ccae
Vinicius Valim <viniciusvalim@minidevinicius.cbnet.com.br>
2022-07-12 08:57:00 -0300

    Aula 2 terminada e 3 iniciada

---
+++ JavaScript Basico /Aulas terminadas/Aula2.js
@@ -0,0 +1,6 @@
+alert("iremoa somar 2 números");
+
+let numberOne = prompt("Digite o primeiro número:");
+let numberTwo = prompt("Digite o segundo número:");
+let result = Number(numberOne) + Number(numberTwo)
+alert("Resultado final: " + result)
